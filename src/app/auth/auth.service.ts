e857e57ff170d2753a839612e72840d2c7a42cb1
wayson <wayson1225@outlook.com>
2023-01-04 21:31:19 -0800

    add signout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -61,4 +61,12 @@ export class AuthService {
         })
       );
   }
+
+  signout() {
+    return this.http.post(this.rootUrl + '/auth/signout', {}).pipe(
+      tap(() => {
+        this.signedIn$.next(false);
+      })
+    );
+  }
 }
