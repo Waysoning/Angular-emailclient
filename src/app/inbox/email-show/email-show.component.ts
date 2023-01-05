f2b2b35c13a1c24fa89207b73ed4dc8e44028495
wayson <wayson1225@outlook.com>
2023-01-05 12:42:34 -0800

    get detailed email and cancel previous requests

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-show',
   templateUrl: './email-show.component.html',
-  styleUrls: ['./email-show.component.css']
+  styleUrls: ['./email-show.component.css'],
 })
 export class EmailShowComponent {
+  constructor(
+    private route: ActivatedRoute,
+    private emailService: EmailService
+  ) {}
 
+  ngOnInit() {
+    // this.route.params.subscribe(({ id }) => {
+    //   this.emailService.getEmail(id).subscribe((email) => {
+    //     console.log(email);
+    //   });
+    // });
+
+    this.route.params
+      .pipe(switchMap(({ id }) => this.emailService.getEmail(id)))
+      .subscribe((email) => {
+        console.log(email);
+      });
+  }
 }
